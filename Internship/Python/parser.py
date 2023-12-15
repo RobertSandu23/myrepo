2216c361b905228b8eb957af7fba758d9a083c13
rsandu <rsandu@Ubuntu2204.myguest.virtualbox.org>
2023-12-15 17:27:21 +0200

    added parser.py

--- Internship/Python/parser.py
+++ Internship/Python/parser.py
@@ -1,4 +1,25 @@
-#Using only python built-in functions,
-#extract the last word of each line between the following timestamp: 17:56:07.996 and 17:56:08.357. Return these in a list. 
+start_timestamp = '17:56:07.996'
+end_timestamp = '17:56:08.357'
+log = 'logcat_applications.txt'
 
-#line 595 - line 1294
+def word_parser(filename, start_timestamp, end_timestamp):
+    last_words = []
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                timestamp = line[6:18]  # take timestamp
+
+                if start_timestamp <= timestamp <= end_timestamp:
+                    words = line.split() # make a list of words
+
+                    if words:
+                        last_word = words[-1] # take las word from the list 
+                        last_words.append(last_word.strip()) # append with removing blank spaces before and after the word
+
+        return last_words
+    
+    except FileNotFoundError:
+        print(f"Couldn't find this file: {log}")         
+
+result = word_parser(log, start_timestamp, end_timestamp)
+print(result)
